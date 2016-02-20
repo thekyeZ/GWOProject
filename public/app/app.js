a5900f5c34e4272ebb7ab10ff75943dae896fb43
Jurek <pawel.j@henouser.pl>
2016-02-21 00:16:29 +0100

    Add "/contact" view and controller (example?)

--- public/app/app.js
+++ public/app/app.js
@@ -1,10 +1,15 @@
 var app = angular.module("gwo", ['ngRoute']);
 
 app.config(["$routeProvider", "$locationProvider", function($routeProvider, $locationProvider) {
+
     $routeProvider.
         when('/', {
             controller : "home",
             templateUrl : "home.html"
+        }).
+        when('/contact', {
+            controller : "contact",
+            templateUrl : "contact.html"
         });
 
     $locationProvider.html5Mode(true);
---
+++ public/app/controllers/contact.js
@@ -0,0 +1,5 @@
+app.controller("contact", ["$scope", function($scope) {
+
+    $scope.title = "Kontakt";
+
+}]);
