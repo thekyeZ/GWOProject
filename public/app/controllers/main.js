a62be72b29407942aa9ab5ce553c6057aeb8658a
Jurek <pawel.j@henouser.pl>
2016-02-25 00:50:17 +0100

    Home page concept with basic angular implementation.

--- public/app/controllers/main.js
+++ public/app/controllers/main.js
@@ -1,7 +1,27 @@
 app.controller("main", ["$scope", function($scope) {
 
-    $scope.site = {
-        title : "GWO Title"
-    };
+    $scope.nav = [
+        {name : 'Dla członków', shortcut: 'dlaczłonków', pos : 0, subMenu : [
+            {name : '2', category : "blog", shortcut: '2', pos : 1},
+            {name : 'lorem', category : "gallery", shortcut: 'lorem', pos : 0}
+        ]},
+        {name : 'Dla kadry', shortcut: 'dlakadry', pos : 1, subMenu : [
+            {name : 'lorem', category : "page", shortcut: 'lorem', pos : 0}
+        ]},
+        {name : 'Dla rodziców', shortcut: 'dlarodziców', pos : 2, subMenu : [
+            {name : '1', category : "blog", shortcut: '1', pos : 0},
+            {name : '2', category : "gallery", shortcut: '2', pos : 1},
+            {name : 'lorem', category : "page", shortcut: 'lorem', pos : 2}
+        ]},
+        {name : 'Bazy', shortcut: 'bazy', pos : 3, subMenu : [
+            {name : '1', category : "page", shortcut: '1', pos : 0},
+            {name : '2', category : "page", shortcut: '2', pos : 1}
+        ]}
+    ];
+
+    $scope.footer = {};
+
+    // used for animation events in DOM
+    $scope.animation = {};
 
 }]);
