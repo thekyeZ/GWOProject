3ec8f1babf856a53e00f3daa47d517b45c959f9d
Przemek Urbaniak <kyez@MBP-Przemek.home>
2016-03-12 21:24:56 +0100

    d

---
+++ public/js/main.js
@@ -0,0 +1,49 @@
+/**
+ * Created by Przemek on 10.12.2015.
+ */
+(function(){
+
+//    slider code
+
+    var wH = $(window).height();
+    var wW = $(window).width();
+
+    var sliderH = $('.slider').height(); //400 by default
+    var imageContainer = $('.slider-images li');
+
+   // $('.slider-images li img').css('width', imageContainer.width());
+   //
+   //$(window).resize(function(){
+   //    $('.slider-images li img').css('width', imageContainer.width());
+   //});
+
+    //$(".slider-images li").each(function(i){
+    //
+    //
+    //});
+    //
+
+
+//    ./slider code
+
+
+
+        var burger = document.querySelector('.burger');
+        var menu = document.querySelector('.main-header');
+        burger.addEventListener("click", function(){
+
+            menu.classList.toggle('active');
+
+
+        });
+
+
+
+
+        //console.log(burger);
+
+
+
+
+
+})();
