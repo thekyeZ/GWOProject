7cfbeab4d24a0d8ba9bbd658b853b9450a9ea1b6
Jurek <pawel.j@henouser.pl>
2016-03-13 04:48:59 +0100

    CREATE post feature added

--- public/admin/app/adminPanel.js
+++ public/admin/app/adminPanel.js
@@ -2,26 +2,27 @@ var app = angular.module("adminPanel", ['ngResource']);
 
 // TODO(jurek) CHECK THIS!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
 app.config(function ($httpProvider) {
-    $httpProvider.defaults.transformRequest = function(data) {
-        if (data === undefined) return data;
+    //$httpProvider.defaults.transformRequest = function(data) {
+    //    if (data === undefined) return data;
+    //
+    //    var fd = new FormData();
+    //    angular.forEach(data, function(value, key) {
+    //        if (value instanceof FileList) {
+    //            if (value.length == 1) {
+    //                fd.append(key, value[0]);
+    //            } else {
+    //                angular.forEach(value, function(file, index) {
+    //                    fd.append(key + '_' + index, file);
+    //                });
+    //            }
+    //        } else {
+    //            fd.append(key, value);
+    //        }
+    //    });
+    //
+    //    return fd;
+    //};
+    //
+    //$httpProvider.defaults.headers.post['Content-Type'] = undefined;
 
-        var fd = new FormData();
-        angular.forEach(data, function(value, key) {
-            if (value instanceof FileList) {
-                if (value.length == 1) {
-                    fd.append(key, value[0]);
-                } else {
-                    angular.forEach(value, function(file, index) {
-                        fd.append(key + '_' + index, file);
-                    });
-                }
-            } else {
-                fd.append(key, value);
-            }
-        });
-
-        return fd;
-    };
-
-    $httpProvider.defaults.headers.post['Content-Type'] = undefined;
 });
