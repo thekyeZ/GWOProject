d382e89287f1da581a73bbaf502084e482f3da70
Paweł Jurkiewicz <pawel.j@henouser.pl>
2016-04-16 16:46:34 +0200

    Add post html render

--- public/app/controllers/post.js
+++ public/app/controllers/post.js
@@ -1,8 +1,9 @@
-app.controller('post', ["$scope", "$routeParams", "$resource", function($scope, $routeParams, $resource) {
+app.controller('post', ["$scope", "$routeParams", "$resource", "$sce", function($scope, $routeParams, $resource, $sce) {
 
     var Post = $resource('/blog/post/:id');
     $scope.post = Post.get({ id : $routeParams.post }, function() {
         console.log(arguments);
+        $scope.post.content = $sce.trustAsHtml($scope.post.content );
     });
 
 }]);
